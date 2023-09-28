b68b3e6d4e4eded1acab1569f97e33ff465da6d8
Ovhas <dovakinbaradat@gmail.com>
2023-09-28 15:23:27 +0200

    Fix open status-bills-container multiple open

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -131,18 +131,15 @@ export default class {
     }
 
     handleShowTickets(e, bills, index) {
-        if (this.counter === undefined || this.index !== index) this.counter = 0
-        if (this.index === undefined || this.index !== index) this.index = index
-        if (this.counter % 2 === 0) {
-            $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)' })
-            $(`#status-bills-container${this.index}`)
-                .html(cards(filteredBills(bills, getStatus(this.index))))
-            this.counter++
+
+        if ($(`#status-bills-container${index}`).data('view')) {
+            $(`#arrow-icon${index}`).css({ transform: 'rotate(90deg)' })
+            $(`#status-bills-container${index}`).html("")
+            $(`#status-bills-container${index}`).data('view', false)
         } else {
-            $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)' })
-            $(`#status-bills-container${this.index}`)
-                .html("")
-            this.counter++
+            $(`#arrow-icon${index}`).css({ transform: 'rotate(0deg)' })
+            $(`#status-bills-container${index}`).html(cards(filteredBills(bills, getStatus(index))))
+            $(`#status-bills-container${index}`).data('view', true)
         }
 
         bills.forEach(bill => {
